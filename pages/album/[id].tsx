ad5ca4bf9701d0e76b1af76e406d00a446a9a8a4
Geremi Ramos <geremiramosm@gmail.com>
2021-11-22 20:33:55 -0400

    Done with most of the Artist's page functionality

---
+++ pages/album/[id].tsx
@@ -0,0 +1,13 @@
+// libraries
+import React from "react";
+
+// components
+import { Artist } from "../../components/Artist/Artist";
+
+//interface
+
+export const artist: React.FC<{}> = ({}) => {
+  return <Artist />;
+};
+
+export default artist;
